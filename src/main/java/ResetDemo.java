bf1c1f48a8a1e5fff80ebf7ce32a12e2eaa83d12
Aida <aida.anarbekova@gmail.com>
2025-07-09 11:31:14 +0600

    Version 1.0.3 --> after reset

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -3,6 +3,7 @@ public class ResetDemo {
         System.out.println("Version 1.0.0");
         System.out.println("Version 1.0.1");
         System.out.println("Version 1.0.2");
-        System.out.println("Version 1.0.3");
+        System.out.println("Version 1.0.3 --> Hello world");
+
     }
 }
